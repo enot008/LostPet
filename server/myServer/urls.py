b56dd4f5574ac1bf75c4c224681a74ce454511e9
enot008 <lexichfiodorov@yandex.ru>
2021-10-20 13:54:18 +0300

    Add base menu

--- server/myServer/urls.py
+++ server/myServer/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
+    path('tasks', views.tasks, name='tasks'),
+    path('maps', views.maps, name='maps'),
+    path('profile', views.profile, name='profile'),
 ]
--- server/myServer/views.py
+++ server/myServer/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'myServer/index.html')
 
+
+def tasks(request):
+    return render(request, 'myServer/tasks.html')
+
+
+def maps(request):
+    return render(request, 'myServer/map.html')
+
+
+def profile(request):
+    return render(request, 'myServer/profile.html')
